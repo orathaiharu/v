f955ca5f136bc870b960ca21bf61f24ab3e8bc6a
Bundit Nuntates <silkyland@gmail.com>
2016-11-26 14:04:04 +0700

    เพิ่ม htaccess เข้าสู่ระบบ

---
+++ application/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lab
+ * Date: 26/11/2559
+ * Time: 13:03
+ */
+class Home extends CI_Controller
+{
+    public function index(){
+        echo "Hello";
+    }
+}
