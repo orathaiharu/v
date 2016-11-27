885fbbc75db8c4514aeaf15d29dc25d9aab7377b
orathaiHaru <nachin.orathai@hotmail.com>
2016-11-27 11:55:02 +0700

    เพิ่ม admin view ใหม่

---
+++ application/controllers/admin/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lab
+ * Date: 27/11/2559
+ * Time: 11:22
+ */
+class Admin extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('admin/admin_view');
+
+    }
+
+
+
+}
